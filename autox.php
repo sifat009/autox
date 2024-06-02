635bf7b5cde515cc9d74cc8e309daf5ae9521315
sifat009 <bhacker150@gmail.com>
2024-06-02 14:03:35 +0600

    feat: Removed readme from plugin-update-checker

--- autox.php
+++ autox.php
@@ -36,8 +36,6 @@ add_action('plugins_loaded', function () {
     //Set the branch that contains the stable release.
     $myUpdateChecker->setBranch('main');
 
-    //Optional: If you're using a private repository, specify the access token like this:
-    // $myUpdateChecker->setAuthentication('your-token-here');
 });
 
 function autox_page()
